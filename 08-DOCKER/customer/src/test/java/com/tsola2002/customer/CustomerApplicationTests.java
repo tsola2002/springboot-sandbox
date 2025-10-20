88bdef246bf92bc79e4cf26805d5dd217e249352
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2025-10-20 14:45:39 +0100

    docker deployments

---
+++ 08-DOCKER/customer/src/test/java/com/tsola2002/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsola2002.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
