585aaf09e2dc392069be6a9afe3afaf68c3fe670
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-02-16 13:42:14 +0100

    databae app

---
+++ 02-DATABASE/mysqlapp/src/test/java/com/tsola2002/demo/IntroappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsola2002.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
