0ef915e1fe322cf4f9be9aaa927ec91f09fef93c
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-05-08 10:23:36 +0100

    added other endpoints

--- 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/controller/CustomerController.java
+++ 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/controller/CustomerController.java
@@ -1,2 +1,47 @@
-package com.tsola2002.jwtapp.controller;public class CustomerController {
+package com.tsola2002.jwtapp.controller;
+
+import com.tsola2002.jwtapp.entity.Customer;
+import com.tsola2002.jwtapp.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // GET ALL CUSTOMERS
+    @GetMapping
+    public List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+
+    // CREATE CUSTOMER
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer){
+        return customerService.createCustomer(customer);
+    }
+
+    // UPDATE CUSTOMER
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id,
+                                   @RequestBody Customer customer){
+        return customerService.updateCustomer(id, customer);
+    }
+
+    // DELETE A CUSTOMER
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomer(id);
+    }
 }
--- 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/service/CustomerService.java
+++ 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/service/CustomerService.java
@@ -12,14 +12,32 @@ public class CustomerService {
   @Autowired
   private CustomerRepository customerRepository;
 
- public List<Customer> getAllCustomers(){
-   return customerRepository.findAll();
+  public List<Customer> getAllCustomers(){
+      return customerRepository.findAll();
  }
 
- public Customer getCustomerById(Long id){
-   return customerRepository.findById(id)
-       .orElseThrow(() -> new RuntimeException("Customer Not Found"));
- }
+  public Customer getCustomerById(Long id){
+       return customerRepository.findById(id)
+           .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+  }
+
+    public Customer createCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(Long id, Customer updatedCustomer){
+        Customer existingCustomer = getCustomerById(id);
+
+        existingCustomer.setName(updatedCustomer.getName());
+        existingCustomer.setEmail(updatedCustomer.getEmail());
+        existingCustomer.setAddress(updatedCustomer.getAddress());
+
+        return customerRepository.save(existingCustomer);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
 
 
 
