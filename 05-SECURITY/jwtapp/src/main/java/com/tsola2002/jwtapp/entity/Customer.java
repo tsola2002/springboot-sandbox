6b4126e69ac11ab78f4d82d14a2f07df32794562
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-05-08 12:16:51 +0100

    added auto increment to colums

--- 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/entity/Customer.java
+++ 05-SECURITY/jwtapp/src/main/java/com/tsola2002/jwtapp/entity/Customer.java
@@ -1,6 +1,8 @@
 package com.tsola2002.jwtapp.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,7 @@ import lombok.Setter;
 public class Customer {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
